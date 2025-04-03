db21726f1a88d4d02726b5ec57959c31374fa327
Abdikman <alma55497@gmail.com>
2025-04-03 10:55:38 +0300

    V2.0 RailWay

--- database/database.go
+++ database/database.go
@@ -2,31 +2,53 @@ package database
 
 import (
 	"fmt"
-	"go-web-server-v2/config"
 	"go-web-server-v2/models"
 	"log"
+	"os"
 
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	// "gorm.io/gorm"
 )
 
 var DB *gorm.DB
 
 func ConnectDB() {
-	// Формируем строку подключения
-	dsn := fmt.Sprintf(
-		"host=%s user=%s password=%s dbname=%s sslmode=disable",
-		config.Config("DB_HOST"),
-		config.Config("DB_USER"),
-		config.Config("DB_PASSWORD"),
-		config.Config("DB_NAME"),
-	)
+	// // Формируем строку подключения
+	// dsn := fmt.Sprintf(
+	// 	"host=%s user=%s password=%s dbname=%s sslmode=disable",
+	// 	config.Config("DB_HOST"),
+	// 	config.Config("DB_USER"),
+	// 	config.Config("DB_PASSWORD"),
+	// 	config.Config("DB_NAME"),
+	// )
 
-	// Подключаемся к базе данных
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// // Подключаемся к базе данных
+	// var err error
+	// DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	// if err != nil {
+	// 	log.Fatal("Ошибка подключения к базе данных:", err)
+	// }
+
+	// Версия для RailWay
+	// Берем DATABASE_URL из переменных окружения
+	connStr := os.Getenv("DATABASE_URL")
+	if connStr == "" {
+		log.Fatal("DATABASE_URL переменная окружения не установлена")
+	}
+
+	// Подключаемся к базе данных через GORM
+	db, err := gorm.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal("Ошибка подключения к базе данных:", err)
+	}
+	defer db.Close()
+
+	// Проверка соединения с базой данных
+	if err := db.DB().Ping(); err != nil {
+		log.Fatal("Не удалось установить соединение с базой данных:", err)
+	} else {
+		fmt.Println("Подключение к базе данных успешно установлено!")
 	}
 
 	// Автоматическая миграция (создаст таблицу users)
--- go.mod
+++ go.mod
@@ -7,6 +7,8 @@ require (
 	gorm.io/gorm v1.25.12
 )
 
+require github.com/lib/pq v1.1.1 // indirect
+
 require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang-jwt/jwt/v4 v4.5.1 // indirect
@@ -15,6 +17,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.5.5 // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
+	github.com/jinzhu/gorm v1.9.16
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	golang.org/x/crypto v0.36.0 // indirect
